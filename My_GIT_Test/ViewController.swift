a56b468173a389f675d0a2457c62a1abb342baf0
Imran Naseem <imran0621mfp@gmail.com>
2023-02-14 20:57:34 +0530

    Initial Commit

---
+++ My_GIT_Test/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  My_GIT_Test
+//
+//  Created by Ayesha Ekram on 14/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("House")
+    }
+
+
+}
+
